e7062411b253f3b3a21c2c1bf4addd30ef79de6d
Bluee11 <71536451+Bluee11@users.noreply.github.com>
2020-09-22 15:08:31 -0500

    Redoing file structure

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Multi Monitor bars
+    for m in $(polybar --list-monitors | cut -d":" -f1); do
+         MONITOR=$m polybar --reload BlueeBar &
+        #MONITOR=$m polybar --reload Date &
+        #MONITOR=$m polybar --reload Stats &
+        #MONITOR=$m polybar --reload Workspaces &
+    done
+
+# Launch
+#polybar Stats &
+#polybar Workspaces &
