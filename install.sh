b0c14ce7b9cad6b792e5b39b7cf876e387711bce
Bluee11 <71536451+Bluee11@users.noreply.github.com>
2020-10-06 22:13:43 -0500

    adding on to the installation script

--- install.sh
+++ install.sh
@@ -1,47 +1,49 @@
 #!/bin/bash
 
-##The goal of this is a personal install script for when I install a minimal Fedora install. Hopefully I shouldn't forget anything.
-#I should be breaking things up so that if I decide that I don't need something, I should be able to comment it out and it should be ignored.
-#I'll probably see if I can expand the script so that it moves my config files to the .config folder.
+## The goal of this script is for installing and setting up a minimal installation of Fedora in such a way that I would consider it "minimal" for my usage.
 
+## this file should be set up in such a way that anyone reading it should be able to comment out lines that they dont't need (ex. if you don't need nvidia graphics drivers, you can comment out the Nvidia drivers install line, and it should still install everything else fine.)
 
 ###Repositories
 
-#This repo allows you to install Alacritty which at the moment is my preferred terminal
+# This repo has Alacritty which is my preferred terminal emulator.
 dnf copr enable pschyska/alacritty -y
 
-#This repo allows you to install i3-gaps instead of the standard i3 that's already in the main Fedora repos
+# This repo has i3-gaps which I prefer over standard i3.
 dnf copr enable fuhrmann/i3-gaps -y
 
-#This should enable the RPMFusion repos
+# This enbales the RPMFusion repos which are needed for software like Nvidia's driverss.
 dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
 ###Software
 
-#This line is for terminal stuff
+# This line installs termainal commands that I use.
 dnf install alacritty bpytop neofetch vim -y
 
-#this line I think installs codecs that stuff like Firefox needs to play videos
+# This line I think installs codecs that stuff like Firefox needs to play videos.
 dnf groupupdate sound-and-video -y
 
-#This line should install Nvidia drivers  
+# This line should install Nvidia drivers. 
 dnf install akmod-nvidia -y
 
-#This should install and set up Lightdm as the display manager.)
+# These commands install and enable lightdm as the display manager for logging in.
 dnf install Xorg lightdm lightdm-slick-greeter -y 
 
 systemctl enable lightdm.service
 systemctl set-default graphical.target
 
-#This should install a file manager, text editor, etc.
-dnf install thunar notepadqq
-#this line should install extra programs like Steam and Discord. If you don't want them you can just remove what you don't want.
+# This installs my window manager setup.
+dnf install i3-gaps rofi polybar -y 
+
+# This line installs fontawesome which I use for icons for my workspaces on polybar
+dnf install fontawesome-fonts -y
+# This installs general software like a file explorer, a text editor, etc.
+dnf install Firefox thunar notepadqq flameshot -y
+
+# This instals Game launchers and chat software like Lutris, Steam, and Discord.
 dnf install steam discord lutris -y 
 
-###Config files
+###Config
 
-#these lines should copy the files from the config folder added by cloning my repo, and placing them in the .config folder
-cp /config/alacritty .config/ -r
-cp /config/i3 .config/ -r
-cp /config/polybar .config/ -r
-cp /config/rofi .config/ -r
+xrandr -s 1920x1080
+
