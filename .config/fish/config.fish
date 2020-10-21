f0e9992219a236d1e6cd767e2e41b05ac3724cad
Bluee11 <71536451+Bluee11@users.noreply.github.com>
2020-10-21 15:57:58 -0500

    Adding fish config since I'm trying that out.

---
+++ .config/fish/config.fish
@@ -0,0 +1,20 @@
+#Vi mode
+fish_vi_key_bindings
+
+##Theme Config
+set -g theme_display_git no
+set -g theme_display_user yes
+set -g theme_title_use_abbreviated_path no
+set -g theme_color_scheme base16-dark
+set -g theme_display_date no
+
+##Aliases
+alias bashtop='bpytop'
+
+alias mic='pactl unload-module module-loopback; pactl load-module module-loopback latency_msec=1'
+
+alias Blueebot='cd ~/Documents/Bluee/BlueeBot; python3 BlueeBotv4.py'
+
+alias EDMC='python3 ~/Documents/Elite\ Dangerous/EDMarketConnector/EDMarketConnector.py'
+
+alias Alt1='cd ~/Documents/Alt1; bash alt1run.sh'
